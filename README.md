e10743bec51e7efbc9b1eb67f65e69458e016ec7
Alessio <alessiofranchi4@gmail.com>
2024-01-24 19:19:38 +0100

    [RM]

--- README.md
+++
@@ -1,2 +0,0 @@
-# task-manager
-Practicing with node and express to build a to-do list application
--- api/db/connect_db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-// Define the function to connect to the DB
-const connectDB = (uriString) => {
-    return mongoose.connect(uriString)
-} 
-
-// Export the function to connect the DB
-module.exports = connectDB
